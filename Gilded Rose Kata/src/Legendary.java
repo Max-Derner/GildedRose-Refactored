0db14f1cd445761c6ec398b25f66597b6985d80c
Max-Derner <93717402+Max-Derner@users.noreply.github.com>
2022-03-12 19:07:03 +0000

    Update Legendary.java

--- Gilded Rose Kata/src/Legendary.java
+++ Gilded Rose Kata/src/Legendary.java
@@ -1,8 +1,8 @@
-public class Legendary extends Item implements Update{
+public class Legendary extends Item{
     public Legendary(String name, int sellIn, int quality) {
         super(name,sellIn,quality);
     }
     public void update(){
-
+        quality=80;
     }
 }
